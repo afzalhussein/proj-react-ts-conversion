d86fbb819229c248d04006529d59705a2a46bcdf
Syed Afzal Hussain <afzalhussein@hotmail.com>
2025-07-19 04:04:58 -0400

    Cleaned up the layout and styling

--- src/components/radio_button/RadioButton.tsx
+++ src/components/radio_button/RadioButton.tsx
@@ -6,18 +6,16 @@ import "./RadioButton.css";
 class RadioButton extends Component<RadioPropsType, RadioState> {
   constructor(props: RadioPropsType) {
     super(props);
-    this.handleResize = this.handleResize.bind(this);
-    let order = props.order;
-    let i = 1;
     this.state = {
-      outerStyle: this.getStyle(4, i),
-      innerStyle: this.getStyle(1, i),
-      selectedStyle: this.getStyle(2, i),
-      taggerStyle: { top: order * 20, width: 25, height: 25 },
+      outerStyle: this.getStyle(4),
+      innerStyle: this.getStyle(1),
+      selectedStyle: this.getStyle(2),
+      // textStyle can be omitted since it's optional
     };
   }
-  getStyle(i: number, m: number) {
-    let value = i * m;
+
+  getStyle(i: number) {
+    const value = i;
     return {
       top: value,
       bottom: value,
@@ -25,44 +23,30 @@ class RadioButton extends Component<RadioPropsType, RadioState> {
       right: value,
     };
   }
-  componentDidMount() {
-    window.addEventListener("resize", this.handleResize);
-  }
-  componentWillUnmount() {
-    window.removeEventListener("resize", this.handleResize);
-  }
-  handleResize(event: any) {
-    let w = 1 + Math.round(window.innerWidth / 300);
-    this.setState({
-      taggerStyle: {
-        top: this.props.order * w * 10,
-        width: w * 10,
-        height: w * 10,
-      },
-      textStyle: { left: w * 13, fontSize: 7 * w },
-    });
-  }
+
   render() {
     return (
-      <div>
-        <div className="radio-tagger" style={this.state.taggerStyle}>
-          <input type="radio" name={this.props.name} id={this.props.id}></input>
-          <label htmlFor={this.props.id}>
-            <div className="radio-text" style={this.state.textStyle}>
-              {this.props.label}
+      <div className="radio-container">
+        <input
+          type="radio"
+          name={this.props.name}
+          id={this.props.id}
+          className="radio-input"
+        />
+        <label htmlFor={this.props.id} className="radio-label">
+          <div className="radio-outer" style={this.state.outerStyle}>
+            <div className="radio-inner" style={this.state.innerStyle}>
+              <div
+                className="radio-selected"
+                style={this.state.selectedStyle}
+              ></div>
             </div>
-            <div className="radio-outer" style={this.state.outerStyle}>
-              <div className="radio-inner" style={this.state.innerStyle}>
-                <div
-                  className="radio-selected"
-                  style={this.state.selectedStyle}
-                ></div>
-              </div>
-            </div>
-          </label>
-        </div>
+          </div>
+          <span className="radio-text">{this.props.label}</span>
+        </label>
       </div>
     );
   }
 }
+
 export default RadioButton;
--- src/components/radio_button_container/RadioButtonContainer.tsx
+++ src/components/radio_button_container/RadioButtonContainer.tsx
@@ -1,7 +1,14 @@
-export default function RadioButtonContainer( { children }: { children: React.ReactNode }) {
+import React from "react";
+import "./RadioButtonContainer.css";
+
+export default function RadioButtonContainer({ children }: { children: React.ReactNode }) {
     return (
         <div className="radio-section">
-            {children}
+            {React.Children.map(children, (child, index) => (
+                <div key={index} className="radio-item">
+                    {child}
+                </div>
+            ))}
         </div>
     );
 }
